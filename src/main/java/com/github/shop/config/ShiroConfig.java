9295068f98ddf4e34120209d8f80dadc405ab271
acgq <chen330021@live.com>
2020-06-24 00:32:27 +0800

    feat: add reids cache manager

--- src/main/java/com/github/shop/config/ShiroConfig.java
+++ src/main/java/com/github/shop/config/ShiroConfig.java
@@ -5,6 +5,9 @@ import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +17,18 @@ import java.util.Map;
 
 @Configuration
 public class ShiroConfig {
-
+    
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    
+    @Value("${spring.redis.port}")
+    private String redisPort;
+    
     @Bean
     public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         shiroFilterFactoryBean.setSecurityManager(securityManager);
+        
         LinkedHashMap<String, String> patternMap = new LinkedHashMap<>();
         patternMap.put("/api/code", "anon");
         patternMap.put("/api/login", "anon");
@@ -26,17 +36,27 @@ public class ShiroConfig {
         patternMap.put("/api/status", "anon");
         patternMap.put("/**", "authc");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(patternMap);
-
+        
         Map<String, Filter> filterMap = new LinkedHashMap<>();
         filterMap.put("authc", new ShiroLoginFilter());
         shiroFilterFactoryBean.setFilters(filterMap);
         return shiroFilterFactoryBean;
     }
-
+    
     @Bean
-    public SecurityManager securityManager(Realm realm) {
+    public SecurityManager securityManager(Realm realm, RedisCacheManager redisCacheManager) {
         DefaultSecurityManager securityManager = new DefaultWebSecurityManager(realm);
+        securityManager.setCacheManager(redisCacheManager);
         return securityManager;
     }
-
+    
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        RedisManager redisManager = new RedisManager();
+        redisManager.setHost(redisHost + ":" + redisPort);
+        RedisCacheManager cacheManager = new RedisCacheManager();
+        cacheManager.setRedisManager(redisManager);
+        return cacheManager;
+    }
+    
 }
