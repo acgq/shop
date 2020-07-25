d89d91800d75761129107c464e9d9a468b31dac8
acgq <chen330021@live.com>
2020-07-25 12:05:20 +0800

    feat: use redis as session dao

--- shop-main/src/main/java/com/github/shop/controller/AuthController.java
+++ shop-main/src/main/java/com/github/shop/controller/AuthController.java
@@ -106,6 +106,7 @@ public class AuthController {
      */
     @GetMapping("/status")
     public StatusResponse getStatus() {
+        System.out.println("轮到我了");
         User user = UserContext.getUser();
         if (user != null) {
             return StatusResponse.loginResponse(user);
